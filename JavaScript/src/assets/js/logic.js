07c4ea7f774f5c540698219613354fb88473e8da
Daniel Salarini <salarini.robotica@gmail.com>
2020-07-09 16:44:26 -0300

    add logic.js

---
+++ JavaScript/src/assets/js/logic.js
@@ -0,0 +1,61 @@
+const display = document.querySelector('.display')
+
+const data = {
+    value1: 0,
+    value2: 0,
+    number: true,
+    operator: '',
+    result: 0,
+    getResult() {
+        console.log(this.operator)
+
+        switch(this.operator) {
+            case '+':
+                this.result = Number(this.value1) + Number(this.value2)
+                break
+            case '-':
+                this.result = Number(this.value1) - Number(this.value2)
+                break
+            case '*':
+                this.result = Number(this.value1) * Number(this.value2)
+                break
+            case '/':
+                this.result = Number(this.value1) / Number(this.value2)
+                break
+        }
+    }
+}
+
+function clean() {
+    display.innerHTML = '0'
+}
+
+function clickButton(value) {
+    if(data.number) {
+        if(display.innerHTML == '0') display.innerHTML = value
+        else display.innerHTML += value
+
+        data.value1 = display.innerHTML
+        console.log('value1) ' + data.value1)
+    } else {
+        if(display.innerHTML == '0') display.innerHTML = value
+        else display.innerHTML += value
+
+        data.value2 = display.innerHTML
+        console.log('value2) ' + data.value2)
+    }
+}
+
+function clickOperator(value) {
+    data.operator = value
+    data.number = false
+    console.log(data.operator)
+
+    clean()
+}
+
+function showResult() {
+    data.getResult()
+    display.innerHTML = data.result
+    data.number = true
+}
