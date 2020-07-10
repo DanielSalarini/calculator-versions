c2014a404a4c82912b8aa4546fcd55eee95f398f
Daniel Salarini <salarini.robotica@gmail.com>
2020-07-10 15:13:36 -0300

    Add sign

--- JavaScript/src/assets/js/logic.js
+++ JavaScript/src/assets/js/logic.js
@@ -36,19 +36,22 @@ function clickButton(value) {
         else display.innerHTML += value
 
         data.value1 = display.innerHTML
+        
         console.log('value1) ' + data.value1)
     } else {
         if(display.innerHTML == '0') display.innerHTML = value
         else display.innerHTML += value
 
         data.value2 = display.innerHTML
+
         console.log('value2) ' + data.value2)
     }
 }
 
 function clickOperator(value) {
     data.operator = value
-    data.number = false
+    data.number = 
+    
     console.log(data.operator)
 
     clean()
@@ -56,6 +59,18 @@ function clickOperator(value) {
 
 function showResult() {
     data.getResult()
+
     display.innerHTML = data.result
     data.number = true
+
+    data.value1 = 0
+    data.value2 = 0
+}
+
+function signal() {
+    const num = Number(display.innerHTML) * -1
+    display.innerHTML = num
+
+    if(data.value2 == 0) data.value1 = num
+    if(data.value1 != 0) data.value2 = num
 }
